5ebcbe3048444466194ce41353e2bd8a2efd1096
garenchan <1412950785@qq.com>
2018-11-11 22:02:25 +0800

    学习如何切换到32位保护模式

--- 操作系统篇/os-tutorial/08-32位保护模式-打印/32bit-print.asm
+++ 操作系统篇/os-tutorial/08-32位保护模式-打印/32bit-print.asm
@@ -9,12 +9,18 @@ print_string_pm:
     mov edx, VEDEO_MEMORY
 
 print_string_pm_loop:
-    mov al, [ebx] ; [ebx] is the address of our character
+    mov al, [ebx] ; ebx is the address of our character
     mov ah, WHITE_ON_BLACK
 
     cmp al, 0 ; check if end of string
     je print_string_pm_done
 
     mov [edx], ax
-    add ebx, 1; next char
-    
+    add ebx, 1 ; next char
+    add edx, 2 ; next video memory position
+
+    jmp print_string_pm_loop
+
+print_string_pm_done:
+    popa
+    ret
